4bc3fdecf2a7b7478efb0f709e4ed5f37acee784
CodeWithAryansh07 <codewitharyansh07@gmail.com>
2025-02-13 20:00:41 +0530

    Minor Changes in Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -48,10 +48,10 @@ const Contact = () => {
         <div className='w-full h-[100vh] md:px-20 py-2 px-1 flex flex-col'>
             <div className='relative h-full rounded-3xl w-full '>
                 <Image src={Terminal} alt="Loading..." className="z-3 w-full h-full object-center" priority />
-                <div className='z-5 top-0 left-0 px-16 py-16 w-full h-full absolute text-white sm:py-20 max-[500px]:top-16'>
+                <div className='z-5 top-0 left-0 px-16 py-16 w-full h-full absolute text-white sm:py-20 max-[400px]:'>
                     <div className='z-5 xl:px-80 md:px-10 '>
-                        <h1 className='text-2xl font-extrabold mb-2 sm:text-4xl'>Let&lsquo;s talk</h1>
-                        <h1 className='text-lg font-medium mb-2 sm:text-xl'>Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to life, I’m here to help.</h1>
+                        <h1 className='text-2xl font-extrabold mb-2 sm:text-4xl max-[400px]:text-xl'>Let&lsquo;s talk</h1>
+                        <h1 className='text-lg font-medium mb-2 sm:text-xl max-[400px]:text-sm'>Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to life, I’m here to help.</h1>
                         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-y-4 mt-5'>
                             <label className='text-xl font-medium xl:my-2'>Name</label>
                             <input type='text' placeholder='Ex. John Doe' {...register('name')} className='bg-transparent border-b-2 border-white py-2 px-4 focus:outline-none focus:border-blue-500' />
@@ -59,9 +59,9 @@ const Contact = () => {
                             <input type='email' placeholder='Ex. john@example.com' {...register('email')} className='bg-transparent border-b-2 border-white py-2 px-4 focus:outline-none focus:border-blue-500' />
                             {/* <label className='text-xl font-medium xl:my-2'>Subject</label>
                             <input type='text' placeholder='Ex. Project Inquiry' {...register('subject')} className='bg-transparent border-b-2 border-white py-2 px-4 focus:outline-none focus:border-blue-500' /> */}
-                            <label className='text-xl font-medium xl:my-2'>Your Message</label>
+                            <label className='text-xl font-medium xl:my-2 '>Your Message</label>
                             <textarea placeholder='Your message' {...register('message')} className='bg-transparent border-b-2 border-white py-2 px-4 focus:outline-none focus:border-blue-500' />
-                            <button type='submit' className='flex flex-row items-center justify-center gap-2 bg-white text-black py-2 text-2xl font-semibold px-4 rounded-lg xl:mt-5'>Send Message<Image src={ArrowUp} alt="Loading..." className="w-4 h-4" /></button>
+                            <button type='submit' className='flex flex-row items-center justify-center gap-2 bg-white text-black py-2 text-2xl font-semibold px-4 rounded-lg xl:mt-5 max-[400px]:text-xl'>Send Message<Image src={ArrowUp} alt="Loading..." className="w-4 h-4" /></button>
                         </form>
                     </div>
                 </div>
